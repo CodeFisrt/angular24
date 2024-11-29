68db8b500f1c0109f16e1ca362a524f25fce3213
codefirst2022 <codefirst2022@gmail.com>
2024-11-29 13:31:02 +0530

    sddfsd

--- src/app/components/control-flow/control-flow.component.ts
+++ src/app/components/control-flow/control-flow.component.ts
@@ -11,8 +11,26 @@ export class ControlFlowComponent {
   isDiv2RedVisiable: boolean =  true;
   dayName: string =  '';
 
+  cityList: string [] = ["Pune","Mumbai","Thane",'Nagpur',"Gondia"]
+
+  studentList: any [] = [
+    {studId:123, isActive: false, name:'AAA',city:'Pune',mobile:'111222222'},
+    {studId:143, isActive: false, name:'BBB',city:'Mumbai',mobile:'22222222'},
+    {studId:234, isActive: true, name:'CCCC',city:'Pune',mobile:'33333333'},
+    {studId:345, isActive: false, name:'DDDD',city:'Jaipur',mobile:'4444444'},
+    {studId:345, isActive: false, name:'EEEE',city:'Jaipur',mobile:'4444444'},
+    {studId:345, isActive: true, name:'FFF',city:'Jaipur',mobile:'4444444'}
+  ]
   hideshowDiv1(isShow: boolean) {
     this.isDiv1Visiable = isShow; 
+     
+    // for (let index = 0; index < array.length; index++) {
+    //   const element = array[index];
+      
+    // }
+
+    
+     
   }
 
   toggleDiv2() {
